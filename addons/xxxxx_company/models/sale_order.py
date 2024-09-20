e9ee7d76ba99a1081694726d680cf60f3ff08c45
JSilva723 <silvajonatanivan@gmail.com>
2024-09-20 15:33:22 -0300

    fix set channel in picking

--- addons/xxxxx_company/models/sale_order.py
+++ addons/xxxxx_company/models/sale_order.py
@@ -9,27 +9,3 @@ class SaleOrder(models.Model):
     @api.onchange('channel_id')
     def _onchange_channel_id(self):
         self.warehouse_id = self.channel_id.warehouse_id
-
-    def action_confirm(self):
-        super(SaleOrder, self).action_confirm()
-        for order in self:
-            order._create_delivery()
-
-    def _create_delivery(self):
-        pickings = self.env['stock.picking']
-        for order in self:
-            picking_vals = order._prepare_picking()
-            picking = pickings.create(picking_vals)
-            picking.action_confirm()
-
-    def _prepare_picking(self):
-        self.ensure_one()
-        picking_type = self.warehouse_id.out_type_id
-        return {
-            'picking_type_id': picking_type.id,
-            'partner_id': self.partner_shipping_id.id,
-            'origin': self.name,
-            'location_dest_id': self.partner_shipping_id.property_stock_customer.id,
-            'location_id': picking_type.default_location_src_id.id,
-            'channel_id': self.channel_id.id,
-        }
--- addons/xxxxx_company/models/stock_picking.py
+++ addons/xxxxx_company/models/stock_picking.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, _
+from odoo import models, fields, _, api
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    channel_id = fields.Many2one(comodel_name='xxxxx_company.sale_channel', string=_('Sale channel'))
+    channel_id = fields.Many2one(comodel_name='xxxxx_company.sale_channel', string=_('Sale channel'), compute='_compute_channel_id')
+
+    @api.depends('origin')
+    def _compute_channel_id(self):
+        SaleOrder = self.env['sale.order'].sudo()
+        for rec in self:
+            channel_id = False
+            if rec.origin:
+                sale_order_id = SaleOrder.search([('name', '=', rec.origin)])
+                if sale_order_id and sale_order_id.channel_id:
+                    channel_id = sale_order_id.channel_id.id
+            rec.channel_id = channel_id
