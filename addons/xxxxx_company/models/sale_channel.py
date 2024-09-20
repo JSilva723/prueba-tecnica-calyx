869aa96eb1a629372b550abb2059e1722f1ccc20
JSilva723 <silvajonatanivan@gmail.com>
2024-09-20 07:04:46 -0300

    raname field journal_id

--- addons/xxxxx_company/models/sale_channel.py
+++ addons/xxxxx_company/models/sale_channel.py
@@ -9,7 +9,7 @@ class SaleChannel(models.Model):
     name = fields.Char(string=_('Name'), required=True, track_visibility='onchange')
     code = fields.Char(string=_('Code'), readonly=True, copy=False, default='New')
     warehouse_id = fields.Many2one(comodel_name='stock.warehouse', string=_('Warehouse'))
-    account_journal_id = fields.Many2one(comodel_name='account.journal', string=_('Journal'))
+    journal_id = fields.Many2one(comodel_name='account.journal', string=_('Journal'))
 
     @api.model
     def create(self, vals):
