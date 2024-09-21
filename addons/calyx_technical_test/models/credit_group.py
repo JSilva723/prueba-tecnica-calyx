cf1c55222a3dd3226d4823bb3bf0eecc846865c6
JSilva723 <silvajonatanivan@gmail.com>
2024-09-20 23:32:22 -0300

    add code with validations

--- addons/calyx_technical_test/models/credit_group.py
+++ addons/calyx_technical_test/models/credit_group.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class CreditGruop(models.Model):
     _name = 'calyx_technical_test.credit_group'
     _description = 'calyx_technical_test.credit_group'
 
+    _sql_constraints = [
+        ('code_unique', 'unique(code)', 'The code must be unique.')
+    ]
+
     name = fields.Char(string=_('Name'), required=True)
+    code = fields.Char(string=_('Code'), required=True)
+
+    @api.constrains('code')
+    def _check_code_restriction(self):
+        for record in self:
+            if '026' in record.code:
+                raise ValidationError('The code cannot contain the sequence 026.')
