e5c4ee82f5947f52e201d0d600a5329f199dbcf9
JSilva723 <silvajonatanivan@gmail.com>
2024-09-21 03:12:30 -0300

    add validations

--- addons/calyx_technical_test/models/res_partner.py
+++ addons/calyx_technical_test/models/res_partner.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, _, api
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     credit_group_ids = fields.Many2many(comodel_name='calyx_technical_test.credit_group', string=_('Credit group'))
     credit_control = fields.Boolean(string=_('Credit control'))
+
+    @api.constrains('credit_control', 'credit_group_ids')
+    def _check_credit_group(self):
+        for partner in self:
+            if partner.credit_control and not partner.credit_group_ids:
+                raise ValidationError('You must assign at least one credit group if credit control is enabled.')
