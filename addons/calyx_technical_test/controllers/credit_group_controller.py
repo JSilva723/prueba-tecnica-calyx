8b91360feaa7c7207b1056bc44062695eb87fe83
JSilva723 <silvajonatanivan@gmail.com>
2024-09-21 18:09:04 -0300

    add create or edit

--- addons/calyx_technical_test/controllers/credit_group_controller.py
+++ addons/calyx_technical_test/controllers/credit_group_controller.py
@@ -1,31 +1,78 @@
 # -*- coding: utf-8 -*-
 import json
-from odoo import http
+import logging
+from odoo import http, _
 from odoo.http import request
 
-import logging
 log = logging.getLogger(__name__)
 
 class CreditGroupController(http.Controller):
-    
-    @http.route('/create_credit_group', type='json', auth='public', methods=['POST'], csrf=False)
-    def create_credit_group(self):
+
+    @http.route('/create_edit_credit_group', type='json', auth='public', methods=['POST'], csrf=False)
+    def create_edit_credit_group(self):
         try:
             body = http.request.jsonrequest
             if not body:
-                return {'status': 400, 'message': 'Invalid JSON format'}
+                return  {'status': 400, 'message': _('Invalid JSON format')}
             
             credit_groups = body['credit_groups']
             if not credit_groups or not isinstance(credit_groups, list):
-                return {'status': 400, 'message': 'Invalid JSON format'}
+                return  {'status': 400, 'message': _('Invalid JSON format')}
             
             for group_data in credit_groups:
                 name = group_data.get('name', '')
                 if not name:
-                    return {'status': 400, 'message': 'Missing group name'}
+                    return  {'status': 400, 'message': _('Missing group name')}
 
-            return {'status': 200, 'message': 'ok'}
-        
+                code = group_data.get('code', '')
+                if not code:
+                    return  {'status': 400, 'message': _('Missing group code')}
+
+                credit_global = group_data.get('credit_global', '')
+                if not credit_global:
+                    return  {'status': 400, 'message': _('Missing group credit global')}
+                try:
+                    credit_global = float(credit_global)
+                except ValueError:
+                    return {'status': 400, 'message': _('The credit global most be number')}
+
+                channel = group_data.get('channel', '')
+                if not channel:
+                    return  {'status': 400, 'message': _('Missing group channel')}
+                try:
+                    channel = int(channel)
+                except ValueError:
+                    return {'status': 400, 'message': _('The channel most be number')}
+
+                # Get channel
+                SaleChannel = http.request.env['calyx_technical_test.sale_channel'].sudo()
+                channel_id = SaleChannel.search([('id', '=', channel)])
+                if not channel_id:
+                    return  {'status': 404, 'message': _(f'Channel not found ({channel})')}
+
+                # Get credit grup
+                CreditGroup = http.request.env['calyx_technical_test.credit_group'].sudo()
+                credit_group_id = CreditGroup.search([('code', '=', code)])
+
+                group = {
+                    'name': name,
+                    'code': code,
+                    'channel_id': channel_id.id,
+                    'credit_global': credit_global,
+                }
+
+                try:
+                    # Edit group
+                    if credit_group_id:
+                        credit_group_id.write(group)
+                    # Create group
+                    else:
+                        credit_group_id.create(group)  
+                except Exception as e:
+                    log.error(f'[ERROR]: {str(e)}')
+                    return {'status': 500, 'message': _('Error on create or edit')}
+
+            return  {'status': 200, 'message': 'ok'}
         except Exception as e:
             log.error(f'[ERROR]: {str(e)}')
-            return {'status': 500, 'message': 'Ups..!'}
+            return {'status': 500, 'message': 'Ups...!'}
